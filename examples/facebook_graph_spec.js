848ff8f6e06fda46b596102c16e14bfb887a3c4b
Vance Lucas <vance@vancelucas.com>
2011-11-08 13:40:12 -0600

    Frisby: Updated 'expectBodyJSONTypes' and 'expectBodyJSONContains' to recurse down keys

--- examples/facebook_graph_spec.js
+++ examples/facebook_graph_spec.js
@@ -34,3 +34,17 @@ frisby.toss('Get Brightbit Facebook Page with custom Accept header')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
 .run();
+
+
+// Test error reponse
+frisby.toss('Get Brightbit Facebook Page Likes')
+  .get('https://graph.facebook.com/111848702235277/likes')
+  .expectStatus(400)
+  .expectBodyJSONTypes('error', {
+    message: String,
+    type: String
+  })
+  .expectBodyJSONContains('error', {
+    type: "OAuthException"
+  })
+.run();
