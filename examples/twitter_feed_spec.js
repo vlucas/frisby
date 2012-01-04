4a610050eaafb6dd887ac4676fab5381c5f1684e
Vance Lucas <vance@vancelucas.com>
2012-01-04 10:25:14 -0600

    More Frisby Jasmine Matcher Tests
    * Added more matcher tests around types and value matches
    * Modified Frisby matchers to better work with Jasmine by returning
      false with custom message instead of throwing exceptions
    * Updated Twitter example

--- examples/twitter_feed_spec.js
+++ examples/twitter_feed_spec.js
@@ -4,20 +4,13 @@
  */
 var frisby = require('../lib/frisby');
 
-// Global setup for all tests
-frisby.globalSetup({
-  request: {
-    headers:{'Accept': 'application/json'}
-  }
-});
-
 
 frisby.create('Get Brightbit Twitter feed')
-  .get('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=brightbit&count=2')
+  .get('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=brightbit')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
   .expectJSON('0', {
-    place: function(val) { expect(val).toMatchOrBeNull("Oklahoma City, OK"); }, // Custom matcher
+    place: function(val) { expect(val).toMatchOrBeNull("Oklahoma City, OK"); }, // Custom matcher callback
     user: {
       verified: false,
       location: "Oklahoma City, OK",
@@ -27,7 +20,7 @@ frisby.create('Get Brightbit Twitter feed')
   .expectJSONTypes('0', {
     id_str: String,
     retweeted: Boolean,
-    in_reply_to_screen_name: function(val) { expect(val).toBeTypeOrNull(String); }, // Custom matcher
+    in_reply_to_screen_name: function(val) { expect(val).toBeTypeOrNull(String); }, // Custom matcher callback
     user: {
       verified: Boolean,
       location: String,
--- spec/frisby_matchers_spec.js
+++ spec/frisby_matchers_spec.js
@@ -1,4 +1,5 @@
-frisby = require('../lib/frisby');
+var frisby = require('../lib/frisby');
+var mockRequest = require('mock-request')
 
 
 describe('Frisby matchers', function() {
@@ -77,6 +78,42 @@ describe('Frisby matchers', function() {
     expect(testJson).toContainJsonTypes({
       test_str: String,
       test_int: Number
+    });
+  });
+
+
+  it('JSONTypes should not match String for integer type', function() {
+    // Expectation
+    expect({
+      test_int: 42,
+    }).not.toContainJsonTypes({
+      test_int: String,
+    });
+  });
+
+
+  it('JSONTypes should not match Number for string type', function() {
+    // Expectation
+    expect({
+      test_str: "I am a string!",
+    }).not.toContainJsonTypes({
+      test_str: Number,
+    });
+  });
+
+
+  it('JSONTypes should not match with non-existent JSON key', function() {
+    // Set fake JSON body
+    var testJson = {
+      test_str: "I am a string!",
+      test_int: 42,
+      test_float: 42.42
+    };
+
+    // Expectation
+    expect(testJson).not.toContainJsonTypes({
+      test_str: String,
+      test: String // Key that does not exist
     });
   });
 
