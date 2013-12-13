cbedb9a6526dc9706a7777ccdc06d8a9d087de39
Vance Lucas <vance@vancelucas.com>
2013-12-13 11:18:52 -0600

    Add path syntax support to expectJSONSchema

--- spec/frisby_json_schema_spec.js
+++ spec/frisby_json_schema_spec.js
@@ -12,7 +12,40 @@ nock('http://example.com', { allowUnmocked: false })
     "name": "A green door",
     "price": 12.50,
     "tags": ["home", "green"]
-  });
+  })
+  .get('/response2')
+  .reply(200, {
+    response: {
+      "id": 1,
+      "name": "A green door",
+      "price": 12.50,
+      "tags": ["home", "green"]
+    }
+  })
+  .get('/response-array')
+  .reply(200, {
+    items: [
+      {
+        "id": 1,
+        "name": "A green door",
+        "price": 12.50,
+        "tags": ["home", "green"]
+      },
+      {
+        "id": 2,
+        "name": "A blue door",
+        "price": 13.50,
+        "tags": ["home", "blue"]
+      },
+      {
+        "id": 3,
+        "name": "A screen door",
+        "price": 21.25,
+        "tags": ["home", "screen", "door"]
+      }
+    ]
+  })
+;
 
 //
 // Tests
@@ -21,7 +54,7 @@ describe('Frisby JSONSchema', function() {
 
   it('should accept and validate JSONSchema object', function() {
     frisby.create(this.description)
-      .get('http://example.com/response1', {foo: 'bar'})
+      .get('http://example.com/response1')
       .expectStatus(200)
       .expectJSONSchema({
         "$schema": "http://json-schema.org/draft-04/schema#",
@@ -58,7 +91,7 @@ describe('Frisby JSONSchema', function() {
 
   it('should accept and validate JSONSchema file with relative path', function() {
     frisby.create(this.description)
-      .get('http://example.com/response1', {foo: 'bar'})
+      .get('http://example.com/response1')
       .expectStatus(200)
       .expectJSONSchema('fixtures/json_schema/response1.json')
     .toss();
@@ -66,9 +99,56 @@ describe('Frisby JSONSchema', function() {
 
   it('should accept and validate JSONSchema file with full root path', function() {
     frisby.create(this.description)
-      .get('http://example.com/response1', {foo: 'bar'})
+      .get('http://example.com/response1')
       .expectStatus(200)
       .expectJSONSchema(path.join(__dirname, 'fixtures/json_schema/response1.json'))
+    .toss();
+  });
+
+  it('should accept and validate JSONSchema object with path syntax', function() {
+    frisby.create(this.description)
+      .get('http://example.com/response2')
+      .expectStatus(200)
+      .expectJSONSchema('response', {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "Product",
+        "description": "A product from Acme's catalog",
+        "type": "object",
+        "properties": {
+          "id": {
+            "description": "The unique identifier for a product",
+            "type": "integer"
+          },
+          "name": {
+            "description": "Name of the product",
+            "type": "string"
+          },
+        },
+      })
+    .toss();
+  });
+
+  it('should accept and validate JSONSchema file with jsonPath syntax', function() {
+    frisby.create(this.description)
+      .get('http://example.com/response2')
+      .expectStatus(200)
+      .expectJSONSchema('response', 'fixtures/json_schema/response1.json')
+    .toss();
+  });
+
+  it('should accept and validate JSONSchema file with jsonPath array syntax', function() {
+    frisby.create(this.description)
+      .get('http://example.com/response-array')
+      .expectStatus(200)
+      .expectJSONSchema('items.*', 'fixtures/json_schema/response1.json')
+    .toss();
+  });
+
+  it('should accept and validate JSONSchema file with jsonPath at least one in array syntax', function() {
+    frisby.create(this.description)
+      .get('http://example.com/response-array')
+      .expectStatus(200)
+      .expectJSONSchema('items.?', 'fixtures/json_schema/response1.json')
     .toss();
   });
 });
